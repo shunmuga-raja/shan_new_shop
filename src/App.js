612fd90158f37c8c7ded19e6ea6017f9756833a1
shammurajas <shunmuga@anjamerica.com>
2024-02-10 20:53:56 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './component/Header'
+import { Outlet } from 'react-router-dom'
+import toast, { Toaster } from 'react-hot-toast';
+import { useEffect } from "react";
+import { setDataProduct } from "./redux/productSlide";
+import { useDispatch, useSelector } from "react-redux";
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch()
+  const productData = useSelector((state)=>state.product)
+ 
+  useEffect(()=>{
+    (async()=>{
+      const res = await fetch(`${process.env.REACT_APP_SERVER_DOMIN}/product`)
+      const resData = await res.json()
+      dispatch(setDataProduct(resData))
+    })()
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+        <Toaster />
+    <div>
+      <Header/>
+      <main className='pt-16 bg-slate-100 min-h-[calc(100vh)]'>
+        <Outlet />
+      </main>
     </div>
-  );
+    
+    </>
+  )
 }
 
-export default App;
+export default App
